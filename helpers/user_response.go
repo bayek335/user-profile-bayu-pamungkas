8fda8e596c1bd9114ff9d468f2e25610d100c14b
bayek335 <bayhek335@gmail.com>
2023-11-19 16:45:49 +0700

    make a function in order to formating user error and succes response api

---
+++ helpers/user_response.go
@@ -0,0 +1,28 @@
+package helpers
+
+import "github.com/bayek335/task-5-pbi-btpns-bayu-pamungkas/app"
+
+// Formating respon api while store data or validation request fail
+func UserResponseFail(err error) *app.UserResponseFail {
+
+	userResFail := &app.UserResponseFail{
+		Success: false,
+		Message: err.Error(),
+	}
+	return userResFail
+
+}
+
+// Formating respon api while store data successed
+func UserResponseSuccess(user *app.User, mes string) *app.UserResponseSuccess {
+
+	userResSuc := &app.UserResponseSuccess{}
+	userResSuc.Success = true
+	userResSuc.Message = "User succesfully " + mes
+	userResSuc.Data.ID = user.ID
+	userResSuc.Data.Email = user.Email
+	userResSuc.Data.Username = user.Username
+
+	return userResSuc
+
+}
