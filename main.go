be4e6b7e865017a4831d21c0cbb58e675b515b0e
bayek335 <bayhek335@gmail.com>
2023-11-19 17:22:29 +0700

    add init function to initiate godotenv and make main funtion pretty clean

--- main.go
+++ main.go
@@ -8,15 +8,19 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func main() {
+func init() {
 	err := godotenv.Load("environment.env")
 	if err != nil {
 		fmt.Println(err)
 		panic("Error while trying to load env")
 	}
 
+}
+func main() {
+	// daabse connection
 	db := database.DBConnection()
 
+	// router
 	router.Routes(db)
 
 }
